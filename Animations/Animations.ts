992990dc0a36f0f1686e67be3b5e392b0ec5522c
Mariyan Brigov <mariyan@evolutionofdreams.com>
2022-07-13 10:20:45 +0300

    Update Animations.ts

--- Animations/Animations.ts
+++ Animations/Animations.ts
@@ -5,30 +5,44 @@ import {
    trigger,
    transition,
    useAnimation,
-} from "@angular/animations"
-
-// Usage
-@Component({
-   selector: "...",
-   templateUrl: "...",
-   styleUrls: ["..."],
-   animations: [Animations.scaleAndOpacity],
-})
-export const Animations = {
-   scaleAndOpacity: trigger("alertAnimation", [
-      transition(":enter", [
-         style({ opacity: 0, transform: "scale(0)" }),
-         animate(
-            "0.2s ease-in-out",
-            style({ opacity: 1, transform: "scale(1)" }),
-         ),
-      ]),
-      transition(":leave", [
-         style({ opacity: 1, transform: "scale(1)" }),
-         animate(
-            "0.2s ease-in-out",
-            style({ opacity: 0, transform: "scale(0)" }),
-         ),
-      ]),
+ } from '@angular/animations';
+ 
+ // Usage
+ /* 
+ @Component({
+    selector: "...",
+    templateUrl: "...",
+    styleUrls: ["..."],
+    animations: [Animations.scaleAndOpacity],
+ })
+  */
+ 
+ export const Animations = {
+   scaleAndOpacity: trigger('alertAnimationScale', [
+     transition(':enter', [
+       style({ opacity: 0, transform: 'scale(0)' }),
+       animate('0.4s ease-in-out', style({ opacity: 1, transform: 'scale(1)' })),
+     ]),
+     transition(':leave', [
+       style({ opacity: 1, transform: 'scale(1)' }),
+       animate('0.4s ease-in-out', style({ opacity: 0, transform: 'scale(0)' })),
+     ]),
    ]),
-}
+   translateAndOpacity: trigger('alertAnimationTranslate', [
+     transition(':enter', [
+       style({ opacity: 0, transform: 'translateY(-15px)' }),
+       animate(
+         '0.4s ease-in-out',
+         style({ opacity: 1, transform: 'translateY(0)' })
+       ),
+     ]),
+     transition(':leave', [
+       style({ opacity: 1, transform: 'translateY(0)' }),
+       animate(
+         '0.4s ease-in-out',
+         style({ opacity: 0, transform: 'translateY(-15px)' })
+       ),
+     ]),
+   ]),
+ };
+ 
