f7d0754c5b8d50aa0622d46c20951cf9f5bfe8db
Mariyan Brigov <mariyan@evolutionofdreams.com>
2022-08-05 12:42:49 +0300

    guards

---
+++ Guards/isNotAuth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotAuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (!localStorage.getItem('auth-token')) {
+      return true;
+    }
+    this.router.navigate(['/home']);
+    return false;
+  }
+}
