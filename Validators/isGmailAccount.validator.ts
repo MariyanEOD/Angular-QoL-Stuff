e08868d67fd3896ec0dd31f71ec25e38b9d19b57
Mariyan Brigov <mariyan@evolutionofdreams.com>
2022-10-25 13:29:59 +0300

    validators add

--- Validators/isGmailAccount.validator.ts
+++ Validators/isGmailAccount.validator.ts
@@ -1,26 +1,10 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms"
-
-/* 
-Usage
-     email: [
-        null,
-        {
-          validators: [Validators.required, isGmailAccount()],
-        },
-      ],
-      reemail: [
-        null,
-        {
-          validators: [Validators.required, isGmailAccount()],
-        },
-      ],
-*/
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function isGmailAccount(): ValidatorFn {
-   return (control: AbstractControl): ValidationErrors | null => {
-      const regex = /(\W|^)[\w.+\-]*@gmail\.com(\W|$)/gi
-      const value = control.value
+  return (control: AbstractControl): ValidationErrors | null => {
+    const regex = /(\W|^)[\w.+\-]*@gmail\.com(\W|$)/gi;
+    const value = control.value;
 
-      return !regex.test(value) ? { gmailAccount: true } : null
-   }
+    return !regex.test(value) ? { gmailAccount: true } : null;
+  };
 }
--- Validators/matchingEmails.validator.ts
+++ Validators/matchingEmails.validator.ts
@@ -2,85 +2,61 @@
 /* eslint-disable @typescript-eslint/dot-notation */
 /* eslint-disable max-len */
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
-
-/*    
-Usage
-  form = this.fb.group(
-    {
-      name: [null, [Validators.required]],
-      username: [null, [Validators.required]],
-      email: [null],
-      reemail: [null,],
-      password: [null, [Validators.required]],
-      repassword: [null, [Validators.required]],
-    },
-    {
-      validator: passAndEmailMatchingValidatior(
-        'password',
-        'repassword',
-        'email',
-        'reemail'
-      ),
-    }
-  );
-
-*/
-
-import { FormGroup } from "@angular/forms"
+import { FormGroup } from '@angular/forms';
 
 export function passAndEmailMatchingValidatior(
-   passwordControlName: string,
-   matchingPasswordControlName: string,
-   emailControlName: string,
-   emailMatchingControlName: string,
+  passwordControlName: string,
+  matchingPasswordControlName: string,
+  emailControlName: string,
+  emailMatchingControlName: string
 ) {
-   return (formGroup: FormGroup) => {
-      const emailRegex =
-         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return (formGroup: FormGroup) => {
+    const emailRegex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-      // The password regex cheks for the current conditions
-      // Minimum eight characters, at least one letter, one number and one special character:
-      const passwordRegex =
-         /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/
+    // The password regex cheks for the current conditions
+    // Minimum eight characters, at least one letter, one number and one special character:
+    const passwordRegex =
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/;
 
-      const password = formGroup.controls[passwordControlName]
-      const matchingPassword = formGroup.controls[matchingPasswordControlName]
-      const passwordErrorObj = {}
-      switch (true) {
-         case password.value !== matchingPassword.value ||
-            password.value === "" ||
-            matchingPassword.value === "":
-            passwordErrorObj["passwordMatchingValidator"] = true
-         case !passwordRegex.test(password.value) ||
-            !passwordRegex.test(matchingPassword.value):
-            passwordErrorObj["passwordRequiredInfo"] = true
-      }
-      if (Object.keys(passwordErrorObj).length > 0) {
-         password.setErrors(passwordErrorObj)
-         matchingPassword.setErrors(passwordErrorObj)
-      } else {
-         password.setErrors(null)
-         matchingPassword.setErrors(null)
-      }
+    const password = formGroup.controls[passwordControlName];
+    const matchingPassword = formGroup.controls[matchingPasswordControlName];
+    const passwordErrorObj = {};
+    switch (true) {
+      case password.value !== matchingPassword.value ||
+        password.value === '' ||
+        matchingPassword.value === '':
+        passwordErrorObj['passwordMatchingValidator'] = true;
+      case !passwordRegex.test(password.value) ||
+        !passwordRegex.test(matchingPassword.value):
+        passwordErrorObj['passwordRequiredInfo'] = true;
+    }
+    if (Object.keys(passwordErrorObj).length > 0) {
+      password.setErrors(passwordErrorObj);
+      matchingPassword.setErrors(passwordErrorObj);
+    } else {
+      password.setErrors(null);
+      matchingPassword.setErrors(null);
+    }
 
-      const email = formGroup.controls[emailControlName]
-      const matchingEmail = formGroup.controls[emailMatchingControlName]
-      let emailErrorObj = { ...email.errors }
-      if (
-         email.value !== matchingEmail.value ||
-         emailRegex.test(email.value) === false ||
-         emailRegex.test(matchingEmail.value) === false
-      ) {
-         emailErrorObj["emailMatchingValidator"] = true
-         email.setErrors(emailErrorObj)
-         matchingEmail.setErrors(emailErrorObj)
-      } else {
-         delete emailErrorObj["emailMatchingValidator"]
-         if (Object.keys(emailErrorObj).length === 0) {
-            emailErrorObj = null
-         }
-         email.setErrors(emailErrorObj)
-         matchingEmail.setErrors(emailErrorObj)
+    const email = formGroup.controls[emailControlName];
+    const matchingEmail = formGroup.controls[emailMatchingControlName];
+    let emailErrorObj = { ...email.errors };
+    if (
+      email.value !== matchingEmail.value ||
+      emailRegex.test(email.value) === false ||
+      emailRegex.test(matchingEmail.value) === false
+    ) {
+      emailErrorObj['emailMatchingValidator'] = true;
+      email.setErrors(emailErrorObj);
+      matchingEmail.setErrors(emailErrorObj);
+    } else {
+      delete emailErrorObj['emailMatchingValidator'];
+      if (Object.keys(emailErrorObj).length === 0) {
+        emailErrorObj = null;
       }
-   }
+      email.setErrors(emailErrorObj);
+      matchingEmail.setErrors(emailErrorObj);
+    }
+  };
 }
