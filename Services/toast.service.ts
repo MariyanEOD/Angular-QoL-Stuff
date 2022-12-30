aad156514fb0d3314e849aa86c27d3505b87b598
Mariyan Brigov <mariyan@evolutionofdreams.com>
2022-12-30 18:03:22 +0200

    toaster and styles

--- Services/toast.service.ts
+++ Services/toast.service.ts
@@ -1,70 +1,69 @@
-import { Injectable } from "@angular/core"
-import { ToastController, ToastOptions } from "@ionic/angular"
+import { Injectable } from '@angular/core';
+import { ToastController, ToastOptions } from '@ionic/angular';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class ToastService {
-   DURATION = 2000
-   constructor(private toastController: ToastController) {}
-   async toast(message: string, duration?: number) {
-      const body: ToastOptions = {
-         position: "top",
-         keyboardClose: true,
-         duration: this.DURATION,
-         buttons: [
-            {
-               role: "cancel",
-               icon: "close-outline",
-            },
-         ],
-         cssClass: "toast-regular",
-      }
-      if (duration) {
-         body.duration = duration
-      }
-      body.message = message
-      const toast = await this.toastController.create(body)
-      await toast.present()
-   }
-   async toastSuccess(message: string, duration?: number) {
-      const body: ToastOptions = {
-         keyboardClose: true,
-         position: "top",
-         duration: this.DURATION,
-         buttons: [
-            {
-               role: "cancel",
-               icon: "close-outline",
-            },
-         ],
-         cssClass: "toast-success",
-         icon: "checkmark-circle-outline",
-      }
-      if (duration) {
-         body.duration = duration
-      }
-      body.message = message
-      const toast = await this.toastController.create(body)
-      await toast.present()
-   }
-   async toastError(message: string, duration?: number) {
-      const body: ToastOptions = {
-         position: "top",
-         keyboardClose: true,
-         buttons: [
-            {
-               role: "cancel",
-               icon: "close-outline",
-            },
-         ],
-         duration: this.DURATION,
-         cssClass: "toast-error",
-         icon: "alert-circle-outline",
-      }
-      if (duration) {
-         body.duration = duration
-      }
-      body.message = message
-      const toast = await this.toastController.create(body)
-      await toast.present()
-   }
+  constructor(private toastController: ToastController) {}
+  async toast(message: string, duration?: number) {
+    const body: ToastOptions = {
+      position: 'top',
+      keyboardClose: true,
+      duration: 4000,
+      buttons: [
+        {
+          role: 'cancel',
+          icon: 'close-outline',
+        },
+      ],
+      cssClass: 'toast-regular',
+    };
+    if (duration) {
+      body.duration = duration;
+    }
+    body.message = message;
+    const toast = await this.toastController.create(body);
+    await toast.present();
+  }
+  async toastSuccess(message: string, duration?: number) {
+    const body: ToastOptions = {
+      keyboardClose: true,
+      position: 'top',
+      duration: 4000,
+      buttons: [
+        {
+          role: 'cancel',
+          icon: 'close-outline',
+        },
+      ],
+      cssClass: 'toast-success',
+      icon: 'checkmark-circle-outline',
+    };
+    if (duration) {
+      body.duration = duration;
+    }
+    body.message = message;
+    const toast = await this.toastController.create(body);
+    await toast.present();
+  }
+  async toastError(message: string, duration?: number) {
+    const body: ToastOptions = {
+      position: 'top',
+      keyboardClose: true,
+      buttons: [
+        {
+          role: 'cancel',
+          icon: 'close-outline',
+        },
+      ],
+      duration: 4000,
+      cssClass: 'toast-error',
+      icon: 'alert-circle-outline',
+    };
+    if (duration) {
+      body.duration = duration;
+    }
+    body.message = message;
+    const toast = await this.toastController.create(body);
+    await toast.present();
+  }
 }
